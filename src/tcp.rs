9923bad40745cb1a8911866065c0a54a507f6a90
Toby Hughes <tobywhughes@gmail.com>
2016-09-29 16:10:28 -0700

    Added documentation to tcp and transactions, and implemented the checksum and header generation. So I guess ' now' I have a valid header. Cause the checksum was fake yesterday. Now its real. Now to make this thing actually do something

--- src/tcp.rs
+++ src/tcp.rs
@@ -7,8 +7,11 @@ use std::thread;
 use std::fs::File;
 use std::str::FromStr;
 
+//Opens up port 24843 on localhost for a tcp connection
 pub fn tcp_listen() {
 	let listener = TcpListener::bind("127.0.0.1:24843").unwrap();
+
+	//Whenever a new tcp connection begins, a new thread is created to handle it
 	for stream in listener.incoming() {
 		match stream {
 			Ok(stream) => {
@@ -24,31 +27,43 @@ pub fn tcp_listen() {
 	drop(listener);
 }
 
+//Connects to an open node
+//Currently requires IP to be given.
+//Eventually will grab different known nodes from the iplog
 pub fn tcp_stream(ip : String) {
 	let port : String= ":24843".to_string();
 	let str = ip + &port;
 	let mut stream = TcpStream::connect(&*str).unwrap();
+	//Test string to send
 	let _ = stream.write(b"foo");
 }
 
+//Handles incoming connections to tcp listener
 fn handle_client(mut stream : TcpStream){
+	//Prints test string
 	let str = &mut String::new();
 	stream.read_to_string(str);
 	println!("{}", str);
+	//Logs ip
 	let ip = stream.peer_addr().unwrap().ip();
 	println!("{}", ip);
 	ip_log(ip.to_string());
 }
 
+//Logs new IP addresses. If the address already exists, ignore it.
 fn ip_log(ip : String){
 	let mut file_exists = false;
+	//If the file opens, it exists. If there is an error, it doesn't.
 	match File::open("iplog.bin"){
 		Ok(attr) => {file_exists = true;},
 		Err(_) => {}
 	};
+
 	if file_exists {
+		//Opens file and saves contents to string vector
 		let mut file = File::open("iplog.bin").unwrap();
 		let mut ipvec : Vec<String> = decode_from(&mut file, bincode::SizeLimit::Infinite).unwrap();
+		//Checks if current IP is in list
 		let mut ip_exists : bool = false;
 		for i in ipvec.clone() {
 			if ip == i {
@@ -56,13 +71,16 @@ fn ip_log(ip : String){
 				break;
 			}
 		}	
+		//If IP doesn't exist, push it to the list. If it does, do nothing.
 		if !ip_exists {
 			ipvec.push(ip);
 		}
+		//Save list back to file
 		let mut c_file = File::create("iplog.bin").unwrap();
 		encode_into(&ipvec, &mut c_file, bincode::SizeLimit::Infinite).unwrap();
 	}
 	else{
+		//Creates an iplog and saves IP as first element
 		let mut file = File::create("iplog.bin").unwrap();
 	        let ipvec = vec!(ip);
 		encode_into(&ipvec, &mut file, bincode::SizeLimit::Infinite).unwrap();	
--- src/transactions.rs
+++ src/transactions.rs
@@ -1,24 +1,37 @@
 extern crate bincode;
 
 use bincode::rustc_serialize::{encode_into, decode_from};
+use crypto::digest::Digest;
+use crypto::sha2::Sha256;
 use std::io::prelude::*;
 use std::fs::File;
 
+//Parses the script into sections that can be run by individual functions
 pub fn parse_trans_script(script : String) {
+	//Grabs the items in the 16 byte header. The header consists of four 4 byte items
+	//A magic number to identify the PieCoin protocol
 	let magic = &script[0..4];
+	//The command to run. In this case we are looking for a transaction command FF10
 	let tcom = &script[4..8];
+	//Size of the payload (minus header and stop) itself, in bytes
 	let psize = &script[8..12];
+	//Checksum of payload. First four bytes of SHA256(SHA256(payload))
 	let pchecksum = &script[12..16];
+	//Evaluates these values at run time to check them against the header
 	let ssize = (script.len() - 20) as u32;
-	let checksum = 0; //until implementation
-	let header_flag : bool = check_header(magic, tcom, psize, pchecksum, ssize, checksum);
+	let checksum = get_checksum((&script[16..script.len() - 4]).to_string());
+	let header_flag : bool = check_header(magic, tcom, psize, pchecksum, ssize, &checksum);
+	//Continues if header is valid
 	if header_flag {
 		println!("######################");
 	}
 }
 
-fn check_header(magic : &str, transcommand : &str, psize : &str, pchecksum : &str, scriptsize : u32, checksum : u32) -> bool{
+//Checks magic number, command, size, and checksum that make up the header
+fn check_header(magic : &str, transcommand : &str, psize : &str, pchecksum : &str, scriptsize : u32, checksum : &str) -> bool{
 	let mut valid : bool = true;
+
+	//If anything doesn't match, set the valid flag to false
 	if magic != "FF00" {
 		valid = false;
 	}
@@ -28,18 +41,38 @@ fn check_header(magic : &str, transcommand : &str, psize : &str, pchecksum : &st
 	if u32::from_str_radix(psize, 16).unwrap() != scriptsize {
 		valid = false;
 	}
-	if u32::from_str_radix(pchecksum, 16).unwrap() != checksum {
+	if pchecksum != checksum {
 		valid = false;
 	}
 	
 	valid
 }
 
-//Testing
+//Creates a simple file for testing
 pub fn create_example_file() {
-	let hex_string : String = "FF00FF1000000000FFFF".to_string();
+	let script : String = "FFFF".to_string();
+	let script_header : String = generate_header(script.clone());
+	let hex_string : String = script_header + &script;
 	let mut file = File::create("extrans.bin").unwrap();
 	encode_into(&hex_string, &mut file, bincode::SizeLimit::Infinite).unwrap();
+}
+
+//Generates a checksum from provided script. Checksum is first 4 bytes of Sha256(Sha256(Payload))
+fn get_checksum(payload : String) ->  String {
+	let mut sha_1 = Sha256::new();
+	let mut sha_2 = Sha256::new();
+	sha_1.input_str(&payload);
+	sha_2.input_str(&sha_1.result_str());
+	(&sha_2.result_str()[0..4]).to_string()
+}
+
+//Generates a transaction header for given script
+fn generate_header(script : String) -> String {
+	let mut sbuf = "FF00".to_string(); //Starts buffer with magic number
+	sbuf = sbuf + "FF10";
+	sbuf = sbuf + &(format!("{:04X}", (script.len()-4)));
+	sbuf = sbuf + &(get_checksum(script[0..script.len()-4].to_string()));
+	sbuf
 }
 
 
